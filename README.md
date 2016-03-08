c372f49f2242c7369f701023a3e810704e10c0c4
mcmo <cmcmordie@charter.net>
2016-03-08 23:46:22 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## File Metadata Microservice
+
+Website: <a href="https://f-file.herokuapp.com/" target="_blank">f-url.herokuapp.com</a>
+
+#### This is a <a href="http://www.freecodecamp.com/mcmo" target="_blank">FreeCodeCamp.com</a> project which uses full-stack JavaScript to accomplish the following objectives.
+
+1.   I can submit a FormData object that includes a file upload.
+2.   When I submit something, I will receive the file size in bytes within the JSON response.
---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict'
+const Hapi = require('hapi')
+const fs = require('fs')
+
+const server = new Hapi.Server()
+server.connection({
+  // required for Heroku
+  port: process.env.PORT || 8080
+})
+
+server.register(require('inert'), (err) => {
+
+  if (err) throw err
+
+  // utilized https://gist.github.com/joyrexus/0c6bd5135d7edeba7b87
+  server.route({
+    method: 'POST',
+    path: '/upload',
+    config: {
+      payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data'
+      },
+      handler: function(request, reply) {
+        var data = request.payload
+        if (data.file) {
+          var name = data.file.hapi.filename;
+          var path = __dirname + "/uploads/" + name;
+          var file = fs.createWriteStream(path);
+
+          file.on('error', function(err) {
+            console.error(err)
+          });
+
+          data.file.pipe(file);
+
+          data.file.on('end', function(err) {
+            fs.stat(path, function(err, stats) {
+              var ret = {
+                filename: data.file.hapi.filename,
+                "content-type": data.file.hapi.headers['content-type'],
+                size: stats.size
+              }
+              reply(ret);
+            })
+          })
+        }
+      }
+    }
+  });
+  
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      reply.file('./index.html')
+    }
+  })
+
+  server.start(() => console.log('Started'))
+})
